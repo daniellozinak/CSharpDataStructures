e34b591b687e5526ba735ba282e4b1941f5ccd93
daniellozinak <55552255+daniellozinak@users.noreply.github.com>
2020-03-31 19:28:05 +0200

    Add files via upload

---
+++ DanielsLinkedList.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class DanielsLinkedList<T> : IEnumerable<T>
+    {
+        private Node<T> _head;
+        private Node<T> _current;
+
+        public T Current => throw new NotImplementedException();
+
+        public DanielsLinkedList()
+        {
+            this._head = null;
+            this._current = _head;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            _current = this._head;
+            while (_current != null)
+            {
+                yield return _current.Item;
+                _current = _current.Next;
+            }
+        }
+
+
+        public void Remove(T item)
+        {
+            if (IsEmpty()) { return; }
+            int i = 0;
+            _current = _head;
+            while (_current.Next != null)
+            {
+                if (_current.Item.Equals(item)) { break; }
+                i++;
+                _current = _current.Next;
+            }
+
+            if (!_current.Item.Equals(item))
+            {
+                return;
+            }
+
+            this.removeFromPos(i+1);
+        }
+
+        public void Add(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            if(IsEmpty())
+            {
+                this._head = node;
+                this._head.Next = null;
+                return;
+            }
+
+            Node<T> temp = this._head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+
+            }
+            temp.Next = node;
+        }
+
+        public void AddToHead(Node<T> node)
+        {
+            if(IsEmpty())
+            {
+                this._head = node;
+                return;
+            }
+
+            node.Next = this._head;
+            this._head = node;
+
+        }
+
+        public void AddToPos(Node<T> node, int pos)
+        {
+            Node<T> temp = this._head;
+            Node<T> prev = null;
+            for(int i = 1; i < pos; i++)
+            {
+                if (temp.Next == null)
+                {
+                    Console.WriteLine("position too high ;)");
+                    return;
+                }
+                prev = temp;
+                temp = temp.Next;
+                
+            }
+
+            prev.Next = node;
+            node.Next = temp;
+        }
+
+        public void RemoveFromTail()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("bruh");
+                return;
+            }
+            Node<T> temp = this._head;
+            Node<T> prev = null;
+
+            while(temp.Next !=null)
+            {
+                prev = temp;
+                temp = temp.Next;
+            }
+            prev.Next = null;
+        }
+
+        public void RemoveFromFront()
+        {
+            this._head = this._head.Next;
+        }
+
+        public void removeFromPos(int pos)
+        {
+            Node<T> temp = this._head;
+            Node<T> prev = temp;
+
+
+            if(pos == 1)
+            {
+                RemoveFromFront();
+                return;
+            }
+            for (int i = 1; i < pos; i++)
+            {
+                if (temp.Next == null)
+                {
+                    Console.WriteLine("position too high ;)");
+                    return;
+                }
+                prev = temp;
+                temp = temp.Next;
+            }
+
+            prev.Next = temp.Next;
+        }
+
+        public bool IsEmpty()
+        {
+            return this._head == null;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("somethings wrong");
+        }
+
+        public bool MoveNext()
+        {
+            if(_current != null && _current.Next != null)
+            {
+                _current = _current.Next;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            _current = _head;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,31 @@
+﻿
+namespace DataStructures
+{
+    class Node<T>
+    {
+        private T _item;
+        private Node<T> _next;
+
+        public Node()
+        {
+            this._next = null;
+        }
+
+        public Node(T item)
+        {
+            this._item = item;
+        }
+
+        public T Item
+        {
+            get { return _item; }
+            set { _item = value; }
+        }
+
+        public Node<T> Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+    }
+}
